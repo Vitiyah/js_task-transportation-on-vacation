995b2a431d4a65413d9dfd06af02674373115e1e
vtyacodes <vitiyah95@gmail.com>
2025-05-07 01:13:36 +0800

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,17 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const singleDayCost = 40;
+
+  if (days >= 3 && days < 7) {
+    return singleDayCost * days - 20;
+  }
+
+  if (days >= 7) {
+    return singleDayCost * days - 50;
+  }
+
+  return singleDayCost * days;
 }
 
 module.exports = calculateRentalCost;
